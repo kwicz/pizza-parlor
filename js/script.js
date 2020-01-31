afb42502decf7ab9294e0b5e5785abcbe784e8ac
Katy Solovewicz <katy@solovewicz.com>
2020-01-31 09:17:05 -0800

    add functionality to accept pizza size and create new instance of pizza from contstructor

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
+// Business Logic
+function Pizza(size){
+	this.size = size;
+	this.toppings = [];
+}
 
+
+// User Logic
+$(document).ready(function(){
+	$("form").submit(function(event){
+		event.preventDefault();
+		var pizzaSize = $("#pizza-size").val();
+		var pizza = new Pizza(pizzaSize);
+		console.log("pizza: " + pizza)
+		 // $.each($("input[name='toppings']:checked"), function(){
+   //              pizza.toppings.push($(this).val());
+	});
 });
