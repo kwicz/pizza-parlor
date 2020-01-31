8f925c31fe2600412627a026f872c32959038b74
Katy Solovewicz <katy@solovewicz.com>
2020-01-31 10:32:53 -0800

    changed html elements to buttons and added baseCost function and showCost function to js file

--- js/script.js
+++ js/script.js
@@ -2,17 +2,41 @@
 function Pizza(size){
 	this.size = size;
 	this.toppings = [];
+	this.price = 0;
 }
 
+// Determine base cost of pizza based on size
+Pizza.prototype.baseCost = function(pizza) {
+	if (pizza.size === "small") {
+		pizza.price = 12;
+	} else if (pizza.size === "medium") {
+		pizza.price = 18;
+	} else {
+		pizza.price = 24;
+	}
+	console.log("pizzaCost: " + pizza.price);
+	
+}
+
+// Push toppings into toppings array
+
+// Determine price of pizza with toppings
+
+// Output total pizza cost to the DOM
+Pizza.prototype.showCost = function(pizza) {
+	$("#results").html("$" + pizza.price + ".00");
+}
 
 // User Logic
-$(document).ready(function(){
-	$("form").submit(function(event){
+$(document).ready(function() {
+	$(".size").click(function() {
 		event.preventDefault();
-		var pizzaSize = $("#pizza-size").val();
+		var pizzaSize = event.target.id;
 		var pizza = new Pizza(pizzaSize);
-		console.log("pizza: " + pizza)
-		 // $.each($("input[name='toppings']:checked"), function(){
-   //              pizza.toppings.push($(this).val());
+		pizza.baseCost(pizza);
+		pizza.showCost(pizza);
+		
 	});
+
+
 });
