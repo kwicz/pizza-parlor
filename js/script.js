f466aa73bd01b84d43158b2f0d739611bacf8c37
Katy Solovewicz <katy@solovewicz.com>
2020-02-03 19:35:43 -0800

    move DOM outputs to user logic, remove pizza as an argument, separate out total cost function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Business Logic
-function Pizza(){
+function Pizza() {
 	this.size = undefined;
 	this.toppings = [];
 	this.sauce = undefined;
@@ -8,90 +8,82 @@ function Pizza(){
 	this.totalPrice = 0;
 }
 
-// Determine base cost of pizza based on size
-Pizza.prototype.addSize = function(pizza, size) {
-	pizza.size = size;
-	if (pizza.size === "small") {
-		pizza.sizePrice = 12;
-	} else if (pizza.size === "medium") {
-		pizza.sizePrice = 18;
+// Determine base cost of user's pizza based on size
+Pizza.prototype.addSize = function(size) {
+	this.size = size;
+	if (this.size === "small") {
+		this.sizePrice = 12;
+	} else if (this.size === "medium") {
+		this.sizePrice = 18;
 	} else {
-		pizza.sizePrice = 24;
+		this.sizePrice = 24;
 	}
-	pizza.showCost(pizza);
-	$(".crustSizes").addClass("hidden");
-	$("#your-" + size).removeClass("hidden");
-	$(".sauces").removeClass("hidden");
+	this.calculateTotal();
 }
 
-// Remove pizza size from user list
-Pizza.prototype.removeSize = function(pizza, size) {
-	$("#your-" + size).addClass("hidden");
-	$(".crustSizes").removeClass("hidden");
-	$(".sauces").addClass("hidden");
-	$(".toppings").addClass("hidden");
-	pizza.sizePrice = 0;
-	pizza.showCost(pizza);
+// Remove pizza crust from user's order
+Pizza.prototype.removeSize = function(size) {
+	this.sizePrice = 0;
+	this.calculateTotal();
 }
 
-// Add sauce to pizza and DOM
-Pizza.prototype.addSauce = function(pizza, sauce) {
-	$("#your-sauce").append("<button class='your-" + sauce + " " + sauce + "' id='" + sauce + "'>" + sauce + "</button>");
-	$(".sauces").addClass("hidden");
-	$(".toppings").removeClass("hidden");
-	pizza.sauce = sauce;
+// Add sauce to user's pizza
+Pizza.prototype.addSauce = function(sauce) {
+	this.sauce = sauce;
 }
 
-// Remove sauce from pizza and DOM
-Pizza.prototype.removeSauce = function(pizza, sauce) {
-	$(".your-" + sauce).addClass("hidden");
-	$(".toppings").addClass("hidden");
-	$(".sauces").removeClass("hidden");
-	pizza.sauce = undefined;
+// Remove sauce from user's pizza
+Pizza.prototype.removeSauce = function(sauce) {
+	this.sauce = undefined;
 }
 
 // Push toppings into pizza toppings array
-Pizza.prototype.addTopping = function(pizza, topping) {
-	$("#" + topping).addClass("hidden");
-	$("#your-toppings").append("<img src='img/" + topping + ".png' alt='" + topping + "' id='" + topping + "'>");
-	pizza.toppings.push(topping);
-	pizza.toppingsPrice++;
-	pizza.showCost(pizza);
+Pizza.prototype.addTopping = function(topping) {
+	this.toppings.push(topping);
+	this.toppingsPrice++;
+	this.calculateTotal();
 }
 
 // Remove toppings from pizza toppings array and adjust cost
-Pizza.prototype.removeTopping = function(pizza, topping) {
-	$("#" + topping).removeClass("hidden");
-	for(i = 0; i < pizza.toppings.length; i++){
-		if (pizza.toppings[i] === topping) {
-			pizza.toppings.splice(i,1);
+Pizza.prototype.removeTopping = function(topping) {
+	for(i = 0; i < this.toppings.length; i++){
+		if (this.toppings[i] === topping) {
+			this.toppings.splice(i,1);
 		}
 	};
-	pizza.toppingsPrice--;
-	pizza.showCost(pizza);
+	this.toppingsPrice--;
+	this.calculateTotal();
 }
 
-// Output total pizza cost to the DOM
-Pizza.prototype.showCost = function(pizza) {
-	pizza.totalPrice = pizza.sizePrice + pizza.toppingsPrice;
-	$("#results").html("$" + pizza.totalPrice + ".00");
-	$(".modal-button").removeClass("hidden");
+// Calculate total pizza cost
+Pizza.prototype.calculateTotal = function() {
+	this.totalPrice = this.sizePrice + this.toppingsPrice;
+	this.showCost();
 }
 
 // User Logic
 $(document).ready(function() {
+	
+	// Preheat the oven
 	var pizza = new Pizza;
 
-	// User selects pizza size
+	// User selects crust size
 	$(".size").click(function() {
-		var pizzaSize = event.target.id;
-		pizza.addSize(pizza, pizzaSize);
+		var size = event.target.id;
+		$(".crustSizes").addClass("hidden");
+		$("#your-" + size).removeClass("hidden");
+		$(".sauces").removeClass("hidden");
+		pizza.addSize(size);
 	});
 
-	// User removes pizza sizes from order list
-	$(".selected").click(function(){
+	// User removes crust size
+	$(".selected").click(function() {
 		var size = $(this).val();
-		pizza.removeSize(pizza, size);
+		$("#your-" + size).addClass("hidden");
+		$(".crustSizes").removeClass("hidden");
+		$(".sauces").addClass("hidden");
+		$(".toppings").addClass("hidden");
+		pizza.removeSize(size);
 	});
 
   // User selects sauce
@@ -100,26 +92,41 @@ $(document).ready(function() {
 			$(".your-" + pizza.sauce).addClass("hidden");
 		}
 		var sauce = event.target.id;
-		pizza.addSauce(pizza, sauce);
+		$("#your-sauce").append("<button class='your-" + sauce + " " + sauce + "' id='" + sauce + "'>" + sauce + "</button>");
+		$(".sauces").addClass("hidden");
+		$(".toppings").removeClass("hidden");
+		pizza.addSauce(sauce);
 	});
 
 	// User removes sauce
-	$("#your-sauce").on("click", function(){
+	$("#your-sauce").on("click", function() {
 		var sauce = event.target.id;
-		pizza.removeSauce(pizza, sauce);
+		$(".your-" + sauce).addClass("hidden");
+		$(".toppings").addClass("hidden");
+		$(".sauces").removeClass("hidden");
+		pizza.removeSauce(sauce);
 	});
 
 	// User selects pizza toppings
 	$(".topping").click(function() {
 		var topping = $(this).val();
-		pizza.addTopping(pizza, topping);
+		$("#" + topping).addClass("hidden");
+		$("#your-toppings").append("<img src='img/" + topping + ".png' alt='" + topping + "' id='" + topping + "'>");
+		pizza.addTopping(topping);
 	});
 
-	// User selects toppings to remove from order
-	$("#your-toppings").on("click", function(){
+	// User removes pizza toppings
+	$("#your-toppings").on("click", function() {
 		var topping = event.target.id;
 		$("img#" + topping).addClass("hidden");
-		pizza.removeTopping(pizza, topping);
+		$("#" + topping).removeClass("hidden");
+		pizza.removeTopping(topping);
 	});
+
+	// Show cost of pizza
+	Pizza.prototype.showCost = function() {
+		$("#results").html("$" + this.totalPrice + ".00");
+		$(".modal-button").removeClass("hidden");
+	}
 
 });
